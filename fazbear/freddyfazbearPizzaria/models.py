464722335253c9d93847722ef39861d7df54495d
Rowbaine <Sumemeleader@gmail.com>
2026-08-09 11:16:25 +0700

    Create database User and Animatronic. Migrate database

--- fazbear/freddyfazbearPizzaria/models.py
+++ fazbear/freddyfazbearPizzaria/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class user(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    usercreated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
+class animatronic(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
